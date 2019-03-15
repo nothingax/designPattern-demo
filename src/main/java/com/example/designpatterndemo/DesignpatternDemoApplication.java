8d0de70bd2e313bbfd8824b993dd1f020a4c1abb
zhangjianwei <CSSVDD@gmail.com>
2019-03-15 13:07:16 +0800

    init

---
+++ src/main/java/com/example/designpatterndemo/DesignpatternDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.designpatterndemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesignpatternDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DesignpatternDemoApplication.class, args);
+    }
+
+}
