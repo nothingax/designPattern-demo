adfa7c179a96b2d2771d98826cc55791331e9f98
zhangjianwei <CSSVDD@gmail.com>
2019-03-17 14:30:27 +0800

    建造者模式demo

--- src/main/java/com/example/demo/builder/Main.java
+++ src/main/java/com/example/demo/builder/Main.java
@@ -9,6 +9,11 @@ import com.example.demo.builder.product.Product;
  * Program Name: designpattern-demo
  * <p>
  * Description: 建造者模式demo
+ * 个人理解：建造者模式不需要接收参数，是程序内部比较固定的永远存在的
+ * 比如游戏中的地图，有低画质与高画质两种建造
+ *
+ * 在本例中，我们建造的产品只关注到部件A、部件B的存在，而不会更进一步关注部件A、B的尺寸、颜色等。
+ * 理解这些很重要，避免陷入对【我该如何传递参数进来？】的纠结。
  * <p>
  *
  * @author zhangjianwei
